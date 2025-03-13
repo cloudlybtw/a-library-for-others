428a095e1ca3166a005c9345f75480047436796a
dakabirov <dakabirov@noreply.platform.alem.school>
2025-03-13 13:54:10 +0000

    revert df9c53a239e54475539cfa597eee4402d692260d
    revert fix: divided functions to files

--- go.mod
+++
@@ -1,3 +0,0 @@
-module a-library-for-others
-
-go 1.22.6
--- main.go
+++ main.go
@@ -1,12 +1,127 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"log"
 	"os"
-
-	"a-library-for-others/csvlib"
 )
+
+type CSVParser interface {
+	ReadLine(r io.Reader) (string, error)
+	GetField(n int) (string, error)
+	GetNumberOfFields() int
+}
+
+var (
+	ErrQuote      = errors.New("excess or missing \" in quoted-field")
+	ErrFieldCount = errors.New("wrong number of fields")
+)
+
+type YourCSVParser struct {
+	fields []string
+	text   string
+	len    int
+}
+
+// func Split(text string) ([]string, error) {
+
+// }
+
+func (parser *YourCSVParser) ReadLine(r io.Reader) (string, error) {
+	char := make([]byte, 1)
+	text := ""
+	var err error
+	array := make([]string, 0)
+	quotesFlag := false
+
+	element := ""
+
+	for {
+		_, err = r.Read(char)
+		if char[0] == '\n' || char[0] == '\r' || err != nil {
+			break
+		}
+		text += string(char[0])
+	}
+	// len 10
+
+	// 0 1 2 3 4 5 6C 7R 8L 9F // i = 6
+	// +3 = 9
+	for i := 0; i < len(text); i++ {
+		if len(element) == 0 && text[i] == '"' { // quoteflag start
+			quotesFlag = true
+			continue
+		}
+
+		if !quotesFlag && text[i] == '"' {
+			err = ErrQuote
+			return "", err
+		}
+
+		if !quotesFlag && text[i] == 'C' { // CRLF check
+			if i+3 < len(text) {
+				if text[i+1] == 'R' && text[i+2] == 'L' && text[i+3] == 'F' {
+					break
+				}
+			}
+		}
+
+		if quotesFlag && text[i] == '"' { // quote and quoteflag end
+			if i+1 < len(text) {
+				if text[i+1] == '"' {
+					i++
+				} else {
+					quotesFlag = false
+					continue
+				}
+			} else {
+				quotesFlag = false
+				continue
+			}
+		}
+
+		if !quotesFlag && text[i] == ',' {
+			array = append(array, element)
+			element = ""
+			continue
+		}
+
+		element += string(text[i])
+
+	}
+	if len(element) != 0 { //strings.TrimSpace(element)
+		array = append(array, element)
+	}
+	if parser.len == 0 {
+		parser.len = len(array)
+	}
+	if len(array) != parser.len {
+		err = ErrFieldCount
+	}
+	if quotesFlag == true {
+		err = ErrQuote
+	}
+	parser.fields = array
+	parser.text = text
+	return parser.text, err
+}
+
+func (parser *YourCSVParser) GetField(n int) (string, error) {
+	// fmt.Println(parser.GetNumberOfFields())
+	if n < 0 || n >= parser.GetNumberOfFields() {
+		return "", ErrFieldCount
+	}
+	return parser.fields[n], nil
+}
+
+func (parser *YourCSVParser) GetNumberOfFields() int {
+	if len(parser.text) == 0 {
+		log.Fatal("Undefined behavior: called before ReadLine is called")
+	}
+	return len(parser.fields)
+}
 
 func main() {
 	file, err := os.Open("example.csv")
@@ -16,10 +131,11 @@ func main() {
 	}
 	defer file.Close()
 
-	var csvparser csvlib.CSVParser = &csvlib.YourCSVParser{}
+	var csvparser CSVParser = &YourCSVParser{}
 
 	for {
 		line, err := csvparser.ReadLine(file)
+
 		if err != nil {
 			if err == io.EOF {
 				break
